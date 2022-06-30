52dd61c39e2537ff4256e20533534fa1ecf2cc66
Mario Guerra <manachoyaso@gmail.com>
2022-06-30 01:12:53 -0400

    start layaout

--- projectAppPhp/resources/views/layaout.blade.php
+++ projectAppPhp/resources/views/layaout.blade.php
@@ -1,16 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="../css/app.css">
+    <link rel="stylesheet" href="{{ asset('/css/app.css')}}">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-    <header>
-        <nav>
-            
+    <header class="header" id="header">
+        <nav class="nav container">
+            <a href="/Home">
+                Projecto App
+            </a>
+            <div class="nav__menu" id="nav__menu">
+                <ul class="nav__list grid">
+                    <li class="nav__item">
+                        <a href="#">
+                            <i class="uil uil-home"></i>
+                            Home
+                        </a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#">
+                            <i class="uil uil-search-alt"></i>
+                            Buscar
+                        </a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#">
+                            <i class="uil uil-user"></i>
+                            Login
+                        </a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#">
+                            <i class="uil uil-users-alt"></i>
+                            Registrarse
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+
+            </div>
         </nav>
     </header>
     <main>
